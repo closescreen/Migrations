73174e4094bc8c6ae7d1481610b97ca6e45e03fe
Dmitry Belyaev <d.belyaev@internest.ru>
2016-12-29 14:46:11 +0300

    check for applicable

--- src/Migrations.jl
+++ src/Migrations.jl
@@ -1,21 +1,21 @@
 """
 julia> Migration() |>
 
-        cache_text( s\"""(m)-> (KH, server) = \"chouse_conf.jl\"|>include \""" )|>
+        cache_text( s\"""(m)-> (KH, server) = "chouse_conf.jl"|>include \""" )|>
           
         check_text( s\""" function (m)
          (KH, server) = cached(m)
-         dbexists = any( _->ismatch( r\"RND600\", _), KH.READONLY( server, \"SHOW DATABASES\")|>readlines )
+         dbexists = any( _->ismatch( r"RND600", _), KH.READONLY( server, "SHOW DATABASES")|>readlines )
         end \""") |>
 
         migrate_text( s\""" function(m)
          (KH, server) = cached(m)
-         KH.MODIFY( server, \"CREATE DATABASE RND600\")
+         KH.MODIFY( server, "CREATE DATABASE RND600")
         end \""", danger=false) |>
 
         rollback_text( s\""" function(m)
          (KH, server) = cached(m)
-         KH.MODIFY( server, \"DROP DATABASE RND600\" )
+         KH.MODIFY( server, "DROP DATABASE RND600" )
         end \""") |> migrate 
         
         # or ... |> rollback( DANGER=true)
@@ -24,18 +24,19 @@ julia> Migration() |>
 """
 module Migrations
 
+typealias Str AbstractString
 
 abstract Action
 
 immutable NotImplemented<:Action end
-
+typealias NotImp NotImplemented
 
 immutable Implemented<:Action
  expr::Expr
  func::Function
  danger::Bool
  
- function Implemented( t::AbstractString; danger=false)
+ function Implemented( t::Str, applicable_to_args::Tuple; danger=false)
   try ex = parse(t)
     
     if isa( ex, Expr)
@@ -45,9 +46,14 @@ immutable Implemented<:Action
     end 
     f = eval(ex)
     
-    if !isa( f, Function)
-        error("Bad type: $(f|>typeof). Must be a Function. \nExpr: $ex")
-    end
+    !isa( f, Function) && error("Bad type: $(f|>typeof). Must be a Function. \nExpr: $ex")
+    
+    if !applicable( f, applicable_to_args... ) 
+        types = map( typeof, applicable_to_args)
+        mets = methods(f)
+        warn("Must be applicable to argument types: $types.\nNow only applicable to: $mets.")
+        error("Creation of Implemented() was stopped.")
+    end        
     
     !danger && ismatch( r"\bDROP\b", t) &&
         "Found smth dangerous in text:\n$t\nYou must mark action as danger=true"|>error
@@ -58,6 +64,7 @@ immutable Implemented<:Action
  end
 end
 
+typealias Imp Implemented
 
 abstract Cache
 
@@ -79,20 +86,24 @@ immutable Migration
  migrate::Action
  rollback::Action
 end 
-export Migration
+
+"typealias Mig == Migration"
+typealias Mig Migration
+
+export Mig, Migration
 
 
-Migration(; cache::Action=NotImplemented(),
+Mig(; cache::Action=NotImp(),
             cached::Cache=EmptyCache(),
-            check::Action=NotImplemented(),
-            migrate::Action=NotImplemented(),
-            rollback::Action=NotImplemented() ) = Migration( cache, cached, check, migrate, rollback )
+            check::Action=NotImp(),
+            migrate::Action=NotImp(),
+            rollback::Action=NotImp() ) = Mig( cache, cached, check, migrate, rollback )
 
 
 """
 Migrations.Migration() |> cache_text(\"""(m)->true\""")
 """
-cache_text( t::AbstractString) = (m::Migration)->cache_text( m, t)
+cache_text( t::Str) = (m::Mig)->cache_text( m, t)
 export cache_text
 
 
@@ -101,15 +112,15 @@ m = Migrations.Migration();
 
 cache_text( m, \"""(m)->info(\"hello from migration\")\""" )
 """
-cache_text( m::Migration, t::AbstractString) = 
-    Migration( cache=Implemented(t), cached=m.cached, check=m.check, migrate=m.migrate, rollback=m.rollback)
+cache_text( m::Mig, t::Str) = 
+    Mig( cache=Imp( t, (m,)), cached=m.cached, check=m.check, migrate=m.migrate, rollback=m.rollback)
 
 
 
 """
 expr::Expr = m|>cache_text # return 'cache' action from migration
 """
-cache_text( m::Migration) = expr( m.cache)
+cache_text( m::Mig) = expr( m.cache)
 
 
 
@@ -117,7 +128,7 @@ cache_text( m::Migration) = expr( m.cache)
 """
 Migrations.Migration() |> check_text(\"""(m)->true\""")
 """
-check_text( t::AbstractString) = (m::Migration)->check_text( m, t)
+check_text( t::Str) = (m::Mig)->check_text( m, t)
 export check_text
 
 
@@ -126,23 +137,23 @@ m = Migrations.Migration();
 
 check_text( m, \"""(m)->info(\"hello from migration\")\""" )
 """
-check_text( m::Migration, t::AbstractString) = 
-    Migration( cache=m.cache, cached=m.cached, check=Implemented(t), migrate=m.migrate, rollback=m.rollback)
+check_text( m::Mig, t::Str) = 
+    Mig( cache=m.cache, cached=m.cached, check=Imp( t, (m,)), migrate=m.migrate, rollback=m.rollback)
 
 
 
 """
 expr::Expr = m|>check_text
 """
-check_text( m::Migration) = expr( m.check)
+check_text( m::Mig) = expr( m.check)
 
 
 
 """
 Migrations.Migration() |> migrate_text(\"""(m)->info(\"hello from migration\")\""")
 """
-migrate_text( t::AbstractString; danger::Bool=true ) = 
-    (m::Migration)->migrate_text( m, t, danger=danger)
+migrate_text( t::Str; danger::Bool=true ) = 
+    (m::Mig)->migrate_text( m, t, danger=danger)
 export migrate_text
 
 
@@ -151,34 +162,34 @@ m = Migrations.Migration();
 
 migrate_text( m, \"""(m)->info(\"hello from migration\")\""" )
 """
-migrate_text( m::Migration, t::AbstractString; danger::Bool=true) = 
-    Migration( cache=m.cache, cached=m.cached, check=m.check, migrate=Implemented(t,danger=danger), rollback=m.rollback)
+migrate_text( m::Mig, t::Str; danger::Bool=true) = 
+    Mig( cache=m.cache, cached=m.cached, check=m.check, migrate=Imp( t, (m,), danger=danger), rollback=m.rollback)
 
 
 
 """
 expr::Expr = m|>migrate_text
 """
-migrate_text( m::Migration) = expr( m.migrate)
+migrate_text( m::Mig) = expr( m.migrate)
 
 
 
-expr( ni::NotImplemented) = ni
-expr( imp::Implemented) = imp.expr
+expr( ni::NotImp) = ni
+expr( imp::Imp) = imp.expr
 
 
 
 """
 expr::Expr = m|>rollback_text
 """
-rollback_text( m::Migration) = expr( m.rollback)
+rollback_text( m::Mig) = expr( m.rollback)
 export rollback_text
 
 
 """
 Migrations.Migration() |> rollback_text(\"""(m)->info(\"hello from rollback\")\""")
 """
-rollback_text( t::AbstractString; danger::Bool=true) = (m::Migration)->rollback_text( m, t, danger=danger)
+rollback_text( t::Str; danger::Bool=true) = (m::Mig)->rollback_text( m, t, danger=danger)
 
 
 
@@ -187,8 +198,8 @@ m = Migrations.Migration();
 
 rollback_text( m, \"""(m)->info(\"hello from migration\")\""" )
 """
-rollback_text( m::Migration, t::AbstractString; danger::Bool=true) = 
-    Migration( cache=m.cache, cached=m.cached, check=m.check, migrate=m.migrate, rollback=Implemented( t, danger=danger))
+rollback_text( m::Mig, t::Str; danger::Bool=true) = 
+    Mig( cache=m.cache, cached=m.cached, check=m.check, migrate=m.migrate, rollback=Imp( t, (m,), danger=danger))
 
 
 
@@ -197,12 +208,12 @@ Returns old m::Migration if cache() not implemented, and return new with cached 
 
 m|>cache
 """
-function cache( m::Migration; debug::Bool=false)::Migration
+function cache( m::Mig; debug::Bool=false)::Mig
  if isa( m.cached, EmptyCache )
     rv = doit( m.cache, m, debug=debug, prefix="Run cache()")
-    isa( rv, NotImplemented ) && return m
+    isa( rv, NotImp ) && return m
     isa( rv, Void ) && error("cache() not return any value! Must return something.\n$(m.cache.expr)")
-    Migration( cache=m.cache, cached=FullCache(rv), check=m.check, migrate=m.migrate, rollback=m.rollback )
+    Mig( cache=m.cache, cached=FullCache(rv), check=m.check, migrate=m.migrate, rollback=m.rollback )
  else
     m
  end
@@ -211,33 +222,35 @@ export cache
 
 
 "Returns migrations cached value (returned from cache())"
-cached(m::Migration) = isa( m.cached, FullCache) ?
+cached(m::Mig) = isa( m.cached, FullCache) ?
     m.cached.value :
-        isa( m.cache, NotImplemented ) ?
+        isa( m.cache, NotImp ) ?
             error("Migration has not cached value, because cache() not implemented.") : 
             error("Migration has not cached value. Forgot to call cache()? cache expression:\n $(m.cache.expr)")
 export cached
 
 
 
-"""
-Checks is migration successed.
+"""Checks is migration successed.
 
 m|>check"""
-function check( m::Migration; debug::Bool=false, prefix::AbstractString="Run check()")
+function check( m::Mig; debug::Bool=false, prefix::Str="check()")
  isa( m.cached, EmptyCache) && ( m = cache( m, debug=debug))
  rv = doit( m.check, m, debug=debug, prefix=prefix)
  if isa( rv, Bool) 
     debug && info("Return: $rv")
     return rv
  end
- isa( rv, NotImplemented ) && error("Not implemented check() for migration $m")
+ isa( rv, NotImp ) && error("Not implemented check() for migration $m")
  error("Bad type $(rv|>typeof) of returned value ($rv). Must be a Bool. check():\n$(m.check.expr)")
 end 
 export check
 
 
-function migrate( m::Migration; checking::Bool=true, DANGER::Bool=false, debug::Bool=true )
+check(;debug::Bool=false, prefix::Str="check()") = (m::Mig)->check( m, debug=debug, prefix=prefix)
+
+
+function migrate( m::Mig; checking::Bool=true, DANGER::Bool=false, debug::Bool=true )
     isa( m.cached, EmptyCache) && (m = cache( m, debug=debug))
     if checking
         chk1 = check( m, debug=true, prefix="Check before migrate()")::Bool
@@ -259,8 +272,8 @@ m|>migrate
 
 m|>migrate( check=false) # without check()
 
-does migrate if check returns false"""
-migrate( ;checking::Bool=true, DANGER::Bool=false, debug::Bool=true) = (m)->migrate( m, checking=checking, DANGER=DANGER, debug=debug)            
+does migrate if check returns false """
+migrate( ;checking::Bool=true, DANGER::Bool=false, debug::Bool=true) = (m::Mig)->migrate( m, checking=checking, DANGER=DANGER, debug=debug)            
 
 
 
@@ -268,8 +281,8 @@ migrate( ;checking::Bool=true, DANGER::Bool=false, debug::Bool=true) = (m)->migr
 
 rollback( m, check=false) # without check()
 
-does rollback if check() returns true"""
-function rollback( m::Migration; checking::Bool=true, DANGER::Bool=false, debug::Bool=true)
+does rollback if check() returns true """
+function rollback( m::Mig; checking::Bool=true, DANGER::Bool=false, debug::Bool=true)
     isa( m.cached, EmptyCache) && ( m = cache( m, debug=debug))
     if checking 
         ch1 = check( m, debug=true, debug=debug, prefix="Checking before rollback")::Bool
@@ -289,10 +302,10 @@ export rollback
 
 m|>rollback( check=false)
 """
-rollback( ;checking::Bool=true, DANGER::Bool=false, debug::Bool=true ) = (m)->rollback( m, checking=checking, DANGER=DANGER, debug=debug)
+rollback( ;checking::Bool=true, DANGER::Bool=false, debug::Bool=true ) = (m::Mig)->rollback( m, checking=checking, DANGER=DANGER, debug=debug)
 
  
-function doit( i::Implemented, args... ; DANGER=false, debug::Bool=false, prefix::AbstractString="" )
+function doit( i::Imp, args... ; DANGER=false, debug::Bool=false, prefix::Str="" )
     !DANGER && i.danger && error("Canceled dangerous action. Must be called with DANGER=true.\n$(i.expr)")
     debug && info("$prefix call:\n$(i.expr)")
     try rv = i.func( args...)
@@ -303,7 +316,7 @@ function doit( i::Implemented, args... ; DANGER=false, debug::Bool=false, prefix
     end
 end        
         
-doit( ni::NotImplemented, args...; DANGER=false, debug::Bool=false, prefix::AbstractString="" )::NotImplemented = ni
+doit( ni::NotImp, args...; DANGER=false, debug::Bool=false, prefix::Str="" )::NotImp = ni
 
 
 
